02540585df018ce110a8ae78be7906e5f9197952
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2018-01-09 15:43:31 +0200

    Added new methods in the embedding script

--- testing_bagels/embedder.py
+++ testing_bagels/embedder.py
@@ -1,12 +1,26 @@
 import numpy as np
 from numpy import linalg as ln
 
+def biggest_eigenvalue(m1):
+    return(calculate_eigenvalues(m1)[0])
+
+def calculate_average_degree(m1):
+    degrees = []
+    for i in m1:
+        non_zero = np.count_nonzero(i)
+        degrees.append(non_zero)
+    return(np.mean(degrees))
+
+def calculate_average_distances(m1):
+    distances = []
+    for i in m1:
+        distance = np.sum(i)
+        distances.append(distance)
+    return(np.mean(distances))
+
 def calculate_eigenvalues(m1):
     eigenvalues = ln.eig(m1)[0]
     return(eigenvalues)
-
-def biggest_eigenvalue(m1):
-    return(calculate_eigenvalues(m1)[0])
 
 def smallest_eigenvalue(m1):
     return(min(calculate_eigenvalues(m1)))
@@ -15,10 +29,10 @@ def sum_eigenvalues(m1):
     return(sum(calculate_eigenvalues(m1)))
 
 def embed(m1):
-    vec = [sum_eigenvalues(m1), biggest_eigenvalue(m1), smallest_eigenvalue(m1)]
+    vec = [calculate_average_degree(m1), calculate_average_distances(m1), sum_eigenvalues(m1), biggest_eigenvalue(m1), smallest_eigenvalue(m1)]
     return(vec)
 
 
-A = np.matrix("1 0 0; 0 1 0; 0 0 1")
+A = np.matrix("1 1 0 0; 1 1 1 0; 0 1 1 1; 0 0 1 1")
 
 print(embed(A))
