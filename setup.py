056d9433baefebfdfea0e0986c79f1ae78665af3
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2018-01-09 13:52:06 +0200

    Project structure added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='testing_bagels',
+      version='0.1',
+      description='Hah',
+      url='http://github.com/baby-age/testing-bagels',
+      packages=['testing_bagels'],
+      zip_safe=False)
