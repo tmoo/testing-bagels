7347006a7dcbfd592a6abab4b7787ad60d9ee25e
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2018-01-09 15:50:07 +0200

    Method for calculating the biggest successive eigengap

--- testing_bagels/embedder.py
+++ testing_bagels/embedder.py
@@ -18,6 +18,16 @@ def calculate_average_distances(m1):
         distances.append(distance)
     return(np.mean(distances))
 
+def calculate_biggest_eigengap(m1):
+    eigs = calculate_eigenvalues(m1)
+    maximum = 0
+    for i in range(len(m1) - 1):
+        gap = eigs[i+1] - eigs[i]
+        if gap > maximum:
+            maximum = gap
+
+    return(maximum)
+
 def calculate_eigenvalues(m1):
     eigenvalues = ln.eig(m1)[0]
     return(eigenvalues)
@@ -29,7 +39,9 @@ def sum_eigenvalues(m1):
     return(sum(calculate_eigenvalues(m1)))
 
 def embed(m1):
-    vec = [calculate_average_degree(m1), calculate_average_distances(m1), sum_eigenvalues(m1), biggest_eigenvalue(m1), smallest_eigenvalue(m1)]
+    vec = [calculate_average_degree(m1), calculate_average_distances(m1),
+     sum_eigenvalues(m1), biggest_eigenvalue(m1), smallest_eigenvalue(m1),
+     calculate_biggest_eigengap(m1)]
     return(vec)
 
 
